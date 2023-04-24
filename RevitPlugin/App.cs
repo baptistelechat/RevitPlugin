740c344c04ffc49c671342e074f5a348045b2ed9
Baptiste LECHAT <baptistelechat@outlook.fr>
2023-04-24 23:36:37 +0200

    ✨ Move CurvesTotalLength.cs to the Command folder and create an App.cs file to generate a custom ribbon with a panel named Tools 🐛 The icon is too big and is outside the allowed area

---
+++ RevitPlugin/App.cs
@@ -0,0 +1,55 @@
+﻿
+using System.Reflection;
+
+using Autodesk.Revit.UI;
+using System.Windows.Media.Imaging;
+
+namespace RevitPlugin
+{
+    class App : IExternalApplication
+    {
+        // define a method that will create our tab and button
+        static void AddRibbonPanel(RibbonPanel panel)
+        {
+            // Get dll assembly path
+            string thisAssemblyPath = Assembly.GetExecutingAssembly().Location;
+
+            // create push button for CurveTotalLength
+            PushButtonData pushButtonData = new PushButtonData(
+                "cmdCurveTotalLength",
+                "Total" + Environment.NewLine + "  Length  ",
+                thisAssemblyPath,
+                "RevitPlugin.Commands.CurvesTotalLength");
+
+            PushButton pushButton = panel.AddItem(pushButtonData) as PushButton;
+            pushButton.ToolTip = "Select Multiple Lines to Obtain Total Length";
+            pushButton.Image =
+                new BitmapImage(new Uri("pack://application:,,,/RevitPlugin;component/Resources/CurvesTotalLength_32x32.png"));
+            pushButton.LargeImage =
+                new BitmapImage(new Uri("pack://application:,,,/RevitPlugin;component/Resources/CurvesTotalLength_32x32.png"));
+            pushButton.ToolTipImage =
+                new BitmapImage(new Uri("pack://application:,,,/RevitPlugin;component/Resources/Cat.gif"));
+        }
+
+        public Result OnStartup(UIControlledApplication application)
+        {
+            // Call our method that will load up our toolbar
+
+            // Create a custom ribbon tab
+            string tabName = "RevitPlugin";
+            application.CreateRibbonTab(tabName);
+
+            // Add a new ribbon panel
+            RibbonPanel ribbonPanelTools = application.CreateRibbonPanel(tabName, "Tools");
+
+            AddRibbonPanel(ribbonPanelTools);
+
+            return Result.Succeeded;
+        }
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            // Do nothing
+            return Result.Succeeded;
+        }
+    }
+}
